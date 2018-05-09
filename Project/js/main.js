39ccdc79948e0bbc3d7870af1074185d059258a1
kefanl <35381104+kefanl@users.noreply.github.com>
2018-05-08 23:36:32 -0400

    Re-submit My Mid-term Project

---
+++ Project/js/main.js
@@ -0,0 +1,90 @@
+function next1() {
+  var x = document.getElementById("slide1");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide2");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function next2() {
+  var x = document.getElementById("slide2");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide3");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function next3() {
+  var x = document.getElementById("slide3");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide4");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function next4() {
+  var x = document.getElementById("slide4");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide5");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function previous5() {
+  var x = document.getElementById("slide5");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide4");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function previous5() {
+  var x = document.getElementById("slide5");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide4");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function previous4() {
+  var x = document.getElementById("slide4");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide3");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function previous3() {
+  var x = document.getElementById("slide3");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide2");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
+function previous2() {
+  var x = document.getElementById("slide2");
+  if (x.style.display !== "none"){
+        x.style.display = "none";
+  }
+  var y = document.getElementById("slide1");
+  if (y.style.display === "none"){
+      y.style.display = "block";
+  }
+};
